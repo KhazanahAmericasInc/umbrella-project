32ec419e2b0dae9e35f236048399d5dd59cee200
Innovation <Innovation@admins-MacBook-Pro.local>
2017-06-07 17:44:35 -0700

    IDs are now packet structs, color mixing implemented

--- umbrella_tranceiver_RF24/umbrella_tranceiver_RF24.ino
+++ umbrella_tranceiver_RF24/umbrella_tranceiver_RF24.ino
@@ -9,9 +9,16 @@
 
 //MAIN PROGRAM GLOBAL PARAMETERS
 
-char ID = 'A'; //IDs should be all unique binary values from 0 to MAX_UMBRELLAS.
+struct packet{
+  char ID;
+  int R;
+  int G;
+  int B;
+};
 
-const int RING_PIN =  12; //ring pin
+packet mypacket;
+
+const int RING_PIN =  5; //ring pin
 const int IDS_PER_SECOND = 100; //how many IDS to send out per second
 
 const float PULSE_LENGTH = 2; //in seconds;
@@ -24,14 +31,16 @@ const float FAR_INTENSITY = 100;//intensity of far color out of 100
 const int MAX_UMBRELLAS = 100; //max amount of umbrellas NOTE: this code uses binary value of ASCII characters.
 char FIRST_ID = 'A'; //the first ID all other IDS must have binary value greater than this and less than MAX_UMBRELLAS
 
+packet rgb_data[MAX_UMBRELLAS]; //store the RGB of each umbrella
+
 const int NUMBER_OF_TESTS = 20; //takes the average signal strength over this many tests
 const float TEST_LENGTH = 0.1f; //length of each test in seconds
 
-int MEDIUM_SIGNAL_AMOUNT = 1 //greater than this and less than CLOSE_SIGNAL_AMOUNT will be considered "medium"
-int CLOSE_SIGNAL_AMOUNT = 2 //greater than this will be considered "close"
+int MEDIUM_SIGNAL_AMOUNT = 1; //greater than this and less than CLOSE_SIGNAL_AMOUNT will be considered "medium"
+int CLOSE_SIGNAL_AMOUNT = 2; //greater than this will be considered "close"
 //less than MEDIUM_SIGNAL_AMOUNT will be considered "far"
 
-float OUTLIER_CONSTANT = 0.8f; //a difference greater than this between tests signals an outlier
+float OUTLIER_CONSTANT = 0.2f; //a difference greater than this between tests signals an outlier
 
 //global temp variables
 float color[3];
@@ -49,6 +58,7 @@ float id_delta = 1.0/IDS_PER_SECOND*1000.0; //how long to wait between sendind I
 
 //LEDs for debugging.
 const bool LED_MODE = true;
+const bool DEBUG_MODE = true;
 
 const int redPin = 2; //pin of red/far led
 const int yellowPin = 3;//pin of yellow/medium led
@@ -66,6 +76,12 @@ Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, RING_PIN); //setup our 16 pixel r
 
 
 void setup() {
+
+  mypacket.ID = 'H'; //IDs should be all unique binary values from 0 to MAX_UMBRELLAS.
+  mypacket.R = 255;
+  mypacket.G = 255;
+  mypacket.B = 255;
+  
  Serial.begin(9600);
  ring.begin(); //init ring
  ring.show(); //show nothing
@@ -80,8 +96,13 @@ void setup() {
  radio.setRetries(5,5); //try to send 100 times with 0 delay.  This doesn't seem to affect things too much.
  radio.setChannel(channel); //set the channel
  printf_begin();
- 
- 
+
+ for (int i = (int)FIRST_ID; i<MAX_UMBRELLAS;i++){
+  rgb_data[i].ID = (char) i;
+  rgb_data[i].R = 0;
+  rgb_data[i].G = 0;
+  rgb_data[i].B = 0;
+ }
 }
 
 void setReadMode(){ //start listening and open our reading pipe
@@ -96,8 +117,8 @@ void setWriteMode(){ //stop listening so we can talk.  Open our writing pipe
 
 
 char captureID(){
-  Serial.println("LISTENING");
-  radio.printDetails();
+  Serial.println("TRYING");
+  if (DEBUG_MODE) radio.printDetails();
   int totals[MAX_UMBRELLAS]; //Array to count instances of each ID
   int past_test[MAX_UMBRELLAS]; //Past test to compare our current one to
   int current_test[MAX_UMBRELLAS]; //The current test
@@ -112,12 +133,15 @@ char captureID(){
   for (int i = 0; i<NUMBER_OF_TESTS; i++){ //How many "tests" to run.  Average is taken of all the tests
     String sample = ""; //empty string
     float t = millis(); //keep track of when we started the test
+    
     while(millis()-t<1000.0*TEST_LENGTH){ //while the time doesn't exceed our defined test length
       doBackgroundStuff(); //see definition
-      char c;
+      packet p;
       setReadMode();
-      radio.read(&c, sizeof(char)); //Write to the address of our character
-      sample.concat(c); //Add on the character to our string
+      radio.read(&p, sizeof(packet)); //Write to the address of our character
+      sample.concat(p.ID); //Add on the character to our string
+      String rgb_string = "";
+      rgb_data[(int)p.ID] = p;
     }
     for (int j = 0; j<sample.length();j++){ //Now we "sort" our string of IDS
       doBackgroundStuff(); //see definition
@@ -128,38 +152,91 @@ char captureID(){
     }
     for (int j = (int)FIRST_ID; j<MAX_UMBRELLAS;j++){
       float difference = current_test[j]-past_test[j];
-      if (Math.fabs(difference)>OUTLIER_CONSTANT){
+      if (fabs(difference)>OUTLIER_CONSTANT*NUMBER_OF_TESTS){
         //DEAL WITH THE OUTLIER
+        if (DEBUG_MODE){
+          Serial.print("CHANGING FROM ");
+          Serial.print(current_test[j]);
+        }
         current_test[j] -= difference/2; //fix the outlier by subtracting the outlier
+        if (DEBUG_MODE){
+          Serial.print(" TO ");
+          Serial.print(current_test[j]);
+          Serial.print(" WHICH IS CLOSER TO ");
+          Serial.println(past_test[j]);
+        }
       }
       totals[j]+=current_test[j];
+      past_test[j]=current_test[j];
+      current_test[j] = 0;
     }
-    
-    
+
   }
 
   //we now know approx how far away everything is (RELATIVELY: AMOUNT OF DATA RECEIVED MIGHT CHANGE WHEN WE ADD MORE NODES)
   //we can do whatever we want with this data, but for now we'll just find the closest umbrella
   
   float highest = 0; //get the highest element in this array we built
+  int indexOfHighest = 0;
     for (int i = (int)FIRST_ID;i<MAX_UMBRELLAS;i++){
       doBackgroundStuff();
       if (totals[i]>highest){
-        highest = totals[i]; //it doesn't really matter the ID of the umbrella.  We just want the closest one
+        highest = totals[i];
+        indexOfHighest = i;
       }
     }
    highest/=NUMBER_OF_TESTS; //average out
    Serial.println(highest); //for debugging
+   Serial.print("COLOR IS ");
+   Serial.println(mypacket.R);
 
    //gets sent to setLight()
    //this is hard coded in.  Would be nicer to have as global variables
    //Or even callibration depending on the total amount of signals we receive
    if (highest<MEDIUM_SIGNAL_AMOUNT) return 'F';
    if (highest<CLOSE_SIGNAL_AMOUNT) return 'M';
-   return 'C';
+   if (highest<3.8) return 'C';
+   combineColor(indexOfHighest);
+   return 'R';
 }
 
+void combineColor(int index){
+  int r1 = mypacket.R;
+  int g1 = mypacket.G;
+  int b1 = mypacket.B;
 
+  int r2 = rgb_data[index].R;
+  int g2 = rgb_data[index].G;
+  int b2 = rgb_data[index].B;
+
+  int rd = r1-r2;
+  int gd = g1-g2;
+  int bd = b1-b2;
+
+  r1-=rd/2;
+  g1-=gd/2;
+  b1-=bd/2;
+
+  mypacket.R = r1;
+  mypacket.G = g1;
+  mypacket.B = b1;
+
+  color[0] = r1;
+  color[1] = g1;
+  color[2] = b1;
+}
+
+String RGBFormat(int input){ //makes sure it's 3 chars long
+  if (input/100>0) return String(input);
+  if (input/10>0){
+    String temp = "0";
+    temp.concat(String(input));
+    return temp;
+  }
+  String temp = "00";
+  temp.concat(String(input));
+  return temp;
+}
 
 void setLight(char level){ //pretty straightforward.  Just setting the color/intensity for close/medium/far
   switch (level){
@@ -195,7 +272,17 @@ void setLight(char level){ //pretty straightforward.  Just setting the color/int
         digitalWrite(yellowPin, LOW);
         digitalWrite(greenPin, HIGH);
       }
+      
       break;
+     case 'R': //R for really close
+       if (LED_MODE){
+          digitalWrite(redPin, HIGH);
+          digitalWrite(yellowPin, HIGH);
+          digitalWrite(greenPin, HIGH);
+        }
+      
+     
+     break;
   }
 }
 
@@ -211,8 +298,8 @@ void doBackgroundStuff(){
 
 void sendID(){
     if ((millis()-last_id_send)>id_delta){ //Logic for timing things right.
-      setWriteMode(); //get into write mode                     
-      radio.write(&ID, sizeof(char)); //send out our ID                   
+      setWriteMode(); //get into write mode               
+      radio.write(&mypacket, sizeof(packet)); //send out our ID                   
       last_id_send = millis(); //track when we sent it
     }
 }
